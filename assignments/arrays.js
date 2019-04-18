19d5109b02e6d9b9c2e12b18a3121fd78c90037d
jtwray <tuckerwray84@gmail.com>
2019-04-17 23:53:40 -0400

    works on arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -62,8 +62,7 @@ let inventory = [{ "id": 1, "car_make": "Lincoln", "car_model": "Navigator", "ca
 // }
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a 2011 jeep wrangler`);
-
+console.log('Car 33 is a ' + `${inventory[33].car_year}` + `${inventory[32].car_make}` + `${inventory[32].car_model}`);
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0;
@@ -91,19 +90,23 @@ for (let i = 0; i < inventory.length; i++) {
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-
-
-
 oldCars = carYears.filter(function(x) {
     return x < 2000;
 });
 console.log(oldCars.length);
-// console.log(oldCars);
+
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 // var new_arr = arr.filter(function(x) {
 let BMWAndAudi = [];
+for (let i = 0; i < inventory.length; i++) {
 
-BMWAndAudi = inventory.filter(function(x) {
-    return inventory.car_make; = "Audi" || "BMW";
-})
+    if (inventory[i].car_make == 'Audi' || inventory[i].car_make == 'BMW') { BMWAndAudi.push(inventory[i]); }
+
+}
+
+// this looks better in the console to me
+console.log(BMWAndAudi);
+
+
+console.log(JSON.stringify(BMWAndAudi));
